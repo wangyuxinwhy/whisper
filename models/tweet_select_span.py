821d90e588b325f0a18598360afaf61a4cd37bb5
yuxin.wang <yuxin.wang@11bee.com>
2020-06-04 19:20:15 +0800

    ignore tweet select span

--- models/tweet_select_span.py
+++ models/tweet_select_span.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
-import logging
-from typing import Any, Dict, List, Optional, Tuple
-
-import torch
-from torch import nn as n
-
-from allennlp.models import Model
-from allennlp.data import Vocabulary
-
-@Model.register("tweet_select_span")
-class TweetSelectSpan(Model):
-    def __init__(
-        self,
-        vocab: Vocabulary,
-        fine_tuned_model_tar_path: str = "",
-        freeze_encoder: bool = True,
-
-    ):
+# import logging
+# from typing import Any, Dict, List, Optional, Tuple
+#
+# import torch
+# from torch import nn as n
+#
+# from allennlp.models import Model
+# from allennlp.data import Vocabulary
+#
+# @Model.register("tweet_select_span")
+# class TweetSelectSpan(Model):
+#     def __init__(
+#         self,
+#         vocab: Vocabulary,
+#         fine_tuned_model_tar_path: str = "",
+#         freeze_encoder: bool = True,
+#
+#     ):
+#         pass
